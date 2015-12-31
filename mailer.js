f4fddaf9d6edc9f4f829285b808562233a627cc7
RichardHan <qwweee7467@gmail.com>
2015-12-31 16:47:01 +0800

    update to sample email account

--- mailer.js
+++ mailer.js
@@ -13,35 +13,14 @@ var mailer = function (opts, fn) {
     
     // Send maail
     try {
-        /* Empty init
-        var transporter = nodemailer.createTransport();
-         */
-
-        /* v1.10.0 
-        var transporter = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-                user: 'qwweee7467@gmail.com',
-                pass: 'amyhwanghabun'
-            }
-        });
-   
-        transporter.sendMail({
-            from: opts.from,
-            to: opts.to,
-            subject: opts.subject,
-            html: opts.body
-        });
-                         
-        */
         /* under v0.7 */
         //You may need to "Allow Less Secure Apps"(https://www.google.com/settings/security/lesssecureapps) in your gmail account(it's all the way at the bottom). 
         //You also may need to "Allow access to your Google account"(https://accounts.google.com/DisplayUnlockCaptcha)
         var transporter = nodemailer.createTransport("SMTP", {
             service: "Gmail",
             auth: {
-                user: "medium.magento@gmail.com",
-                pass: "magento01"
+                user: "UseYourEmailAccount@gmail.com",
+                pass: "UseYourEmailPassword"
             }
         });
         
